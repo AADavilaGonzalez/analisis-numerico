edc5a42b26d795ff132c97bd50c239ba05079cf4
Aldo Davila <aldoda2003gp5@gmail.com>
2025-08-18 14:15:24 -0600

    ajustes a ejemplo grafica

--- ejemplos/grafica.py
+++ ejemplos/grafica.py
@@ -6,29 +6,22 @@ plt = importar_dependencia_opcional(
     "matplotlib", modulo="matplotlib.pyplot", auto=True
 )
 
-FuncAnimation = importar_dependencia_opcional(
-    "matplotlib", modulo="matplotlib.animation",
-    objeto="FuncAnimation", auto=True
-)
+LIMX = (-4, 4)
+LIMY = (-2, 6)
+RES = 1000
 
-if np and plt and FuncAnimation:
-    x = np.linspace(-2*np.pi, 2*np.pi, 1000)
-    y = np.sin(x)
+import math
+f = lambda x : math.exp(x) - math.pi*x
+
+if np and plt:
+    x = np.linspace(LIMX[0]-1, LIMX[1]+1, RES)
+    y = np.vectorize(f)(x)
 
     fig, ax = plt.subplots()
-    sine_line, = ax.plot(x,y)
-
-    def actualizar(frame):
-        mult = np.sin(180*frame/np.pi)
-        sine_line.set_ydata(y+mult)
-        return sine_line,
-
     ax.axhline(0, color="black", linewidth=1)
     ax.axvline(0, color="black", linewidth=1)
-    ax.set_xlim(-np.pi, np.pi)
-    ax.set_ylim(-1.2, 1.2)
+    ax.set_xlim(*LIMX)
+    ax.set_ylim(*LIMY)
     ax.grid(True)
-
-    anim = FuncAnimation(fig, actualizar, frames=100, interval=50, blit=True)
-
+    ax.plot(x,y)
     plt.show()
