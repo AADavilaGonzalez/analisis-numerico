07a1300f52e6bd06838fba9653200742566e83de
Aldo Davila <aldoda2003gp5@gmail.com>
2025-09-04 22:03:41 -0600

    anadir utils.equipo

---
+++ utils/equipo.py
@@ -0,0 +1,7 @@
+# Integrantes:
+# *Aldo Adrian Davila Gonzalez     1994122
+# *Luis Fernando Segobia Torres    2177528
+# *Roberto Sánchez Santoyo         2177547
+# FECHA: <GLUE::DATE>
+# FORMATO: El codigo pertinente a esta actividad se encuentra
+# al final del archivo, delimitado por un comentario
